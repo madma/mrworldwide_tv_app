8018b0099bd024124af3e9e31f991fc1983cbe6f
Michael Duran <mad2116@gmail.com>
2016-02-17 10:18:08 -0800

    add need seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,38 @@
 Video.delete_all
 
 Video.create(
-  embed_url: "EPo5wWmKEaI",
-  title: "Give Me Everything",
-  album: "Planet Pit",
+  embed_url:        "EPo5wWmKEaI",
+  title:            "Give Me Everything",
+  album:            "Planet Pit",
   featured_artists: "Ne-Yo, Afrojack, Nayer",
-  released_on: Date.parse("06-05-2011"),
-  tags: "#dale"
+  released_on:      Date.parse("6-5-2011"),
+  tags:             "#dale"
+)
+
+Video.create(
+  embed_url:        "bTXJQ5ql5Fw",
+  title:            "Time of Our Lives",
+  album:            "Globalization",
+  featured_artists: "Ne-Yo",
+  released_on:      Date.parse("25-12-2011"),
+  tags:             "#dale"
+)
+
+
+Video.create(
+  embed_url:        "SmM0653YvXU",
+  title:            "Rain Over Me",
+  album:            "Planet Pit",
+  featured_artists: "Marc Anthony",
+  released_on:      Date.parse("22-6-2011"),
+  tags:             "#dale #marcanthony"
+)
+
+Video.create(
+  embed_url:        "hHUbLv4ThOo",
+  title:            "Timber",
+  album:            "Meltdown",
+  featured_artists: "Ke$ha",
+  released_on:      Date.parse("25-11-2013"),
+  tags:             "#countrygold"
 )
